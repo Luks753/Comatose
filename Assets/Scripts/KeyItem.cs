bcbc5fc1668a5bc55c1c611da4f5c1c7beb6b826
Perior <theperior@hotmail.com>
2021-07-10 13:27:40 -0300

    Spawn das penas quando inimigo morre [Em andamento]

--- Assets/Scripts/KeyItem.cs
+++ Assets/Scripts/KeyItem.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KeyItem : MonoBehaviour
 {
+    public GameObject[] enemy;
+    public GameObject keyFeather;
+    private int RandomOption;
+    private bool isSpawned = false;
+
+    void Start(){
+        RandomOption = Random.Range(0, enemy.Length);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, .5f);
@@ -11,6 +21,13 @@ public class KeyItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(enemy.Length != 0 && !isSpawned){
+            if(!enemy[RandomOption].activeInHierarchy){
+                Instantiate(keyFeather, enemy[RandomOption].transform.position, Quaternion.identity);
+                isSpawned = true;
+            }
+        }
+        
         var eufuipego = Physics2D.CircleCast(transform.position, .5f, Vector2.zero);
         try
         {
