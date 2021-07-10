2358b01b19c0aa6a9dfe8af429567f55ce31e197
Perior <theperior@hotmail.com>
2021-07-10 19:04:08 -0300

    Sistema de Chaves + HUD Filtro dos Sonhos
    É basicamente isso...

---
+++ Assets/CoinPicker.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+    public GameObject[] empty;
+    public GameObject[] keypart;
+
+    private int counter = 0;
+    private string name;
+    
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.transform.tag == "Key"){
+            name = other.gameObject.name;
+            Destroy(other.gameObject);
+
+            hudChanger(name);
+        }
+    }
+
+    void hudChanger(string name){
+        switch(name)
+        {
+            case "Feather1":
+                keypart[0].SetActive(true);
+                empty[0].SetActive(false);
+                counter++;
+                break;
+            case "Feather2":
+                keypart[1].SetActive(true);
+                empty[1].SetActive(false);
+                counter++;
+                break;
+            case "Key3":
+                keypart[2].SetActive(true);
+                empty[2].SetActive(false);
+                counter++;
+                break;
+            case "Filter4":
+                keypart[3].SetActive(true);
+                empty[3].SetActive(false);
+                counter++;
+                break;
+            default:
+                break;
+        }
+
+        if(counter == 4)
+            Win();
+    }
+
+    void Win(){
+        
+    }
+}
--- Assets/KeySpawn.cs
+++ Assets/KeySpawn.cs
@@ -8,11 +8,14 @@ public class KeySpawn : MonoBehaviour
     public GameObject[] enemyA2;
     public GameObject keyFeatherA1;
     public GameObject keyFeatherA2;
+    public GameObject keyKey;
+    public GameObject keyFilter;
 
     private int RandomOptionA1;
     private int RandomOptionA2;
     private bool A1Spawned;
     private bool A2Spawned;
+    private bool filterSpawned;
 
 
     // Start is called before the first frame update
@@ -28,7 +31,7 @@ public class KeySpawn : MonoBehaviour
         if(enemyA1.Length != 0 && !A1Spawned){
             if(!enemyA1[RandomOptionA1].activeInHierarchy){
                 keyFeatherA1.transform.position = enemyA1[RandomOptionA1].transform.position;
-                //GameObject clone = Instantiate(keyFeatherA1, enemyA1[RandomOptionA1].transform.position, Quaternion.identity);
+                //Instantiate(keyFeatherA1, enemyA1[RandomOptionA1].transform.position, Quaternion.identity);
                 keyFeatherA1.SetActive(true);
                 A1Spawned = true;
             }
@@ -42,5 +45,14 @@ public class KeySpawn : MonoBehaviour
                 A2Spawned = true;
             }
         }
+
+        if(!filterSpawned &&
+           keyFeatherA1 == null &&
+           keyFeatherA2 == null &&
+           keyKey == null)
+           {
+               keyFilter.SetActive(true);
+               filterSpawned = true;
+           }
     }
 }
